27743cffccc0a0c0936f2ac48c6888c8f1fc2842
Kakaique2000 <kaique.lisboa@gmail.com>
2020-08-27 21:16:46 -0300

    initial commit

---
+++ src/main/java/br/com/projetoempregobackend/ProjetoEmpregoBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.projetoempregobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoEmpregoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoEmpregoBackendApplication.class, args);
+	}
+
+}
